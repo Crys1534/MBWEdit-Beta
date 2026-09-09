6a08be100209728ffe123ccc5b9181863d18d43a
Crystal <crystalsnow1534@gmail.com>
2026-09-09 14:36:23 -0600

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("canvas");
-canvas.width = 960;
-canvas.height = 480;
+canvas.width = 1360;
+canvas.height = 680;
 
 const ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false;
@@ -19,8 +19,8 @@ images.names.forEach((name) => {
 });
 
 const grid = {
- width: 60,
- height: 30
+ width: 85,
+ height: 42.5,
 }
 
 let tileSize = canvas.width / grid.width;
